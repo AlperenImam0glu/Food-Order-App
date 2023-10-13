06aad112db9a8887281573b2f4b47959b1151f8b
AlperenImam0glu <alperenimamoglu16@gmail.com>
2023-10-13 03:58:40 +0300

    Base package files created

---
+++ app/src/main/java/com/example/foodorderapp/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.foodorderapp.di
+
+class AppModule {
+}
---
+++ app/src/main/java/com/example/foodorderapp/ui/fragment/MainPageFragment.kt
@@ -0,0 +1,22 @@
+package com.example.foodorderapp.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foodorderapp.R
+
+class MainPageFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_page, container, false)
+    }
+
+
+}
