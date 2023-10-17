8914dae70ae5b4581eea33ccccc01c8c94c2f9f3
AlperenImam0glu <alperenimamoglu16@gmail.com>
2023-10-17 12:59:00 +0300

    added bottom navbar

--- app/src/main/java/com/example/foodorderapp/MainActivity.kt
+++ app/src/main/java/com/example/foodorderapp/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.foodorderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.foodorderapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNav, navHostFragment.navController)
+
     }
 }
